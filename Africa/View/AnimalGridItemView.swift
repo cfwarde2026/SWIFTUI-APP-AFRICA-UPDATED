fffc9d3622718f53dab9be44911e575ab95ae1a2
Cormac Warde <cfwarde@gmail.com>
2026-04-01 02:33:15 -0400

    lesson 123 (end)

---
+++ Africa/View/AnimalGridItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimalGridItemView.swift
+//  Africa
+//
+//  Created by Cormac Warde on 4/1/26.
+//
+
+import SwiftUI
+
+struct AnimalGridItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimalGridItemView()
+}
